176e8b73fb0c96cf93bc6c03026f370445dbc01b
Xalalau Xubilozo <xalalau@outlook.com>
2019-12-12 19:47:23 -0300

    General material browser improvements

--- lua/autorun/client/materialbrowser.lua
+++ lua/autorun/client/materialbrowser.lua
@@ -1,31 +1,35 @@
 local Window
-local MaterialBox
-local List
-local Reload
 local MatRetMaterial = CreateMaterial("MatRetMaterial", "UnlitGeneric", {["$basetexture"] = ""})
 
-local function ParseDir(t, dir, ext)
+local function ParseDir(t, dir, ext, MaterialBox)
 	local files, dirs = file.Find(dir.."*", "GAME")
+
 	for _, fdir in pairs(dirs) do
 		local n = t:AddNode(fdir)
+
 		n:SetExpanded(true)
 		n.DoClick = function()
-			ParseDir(n, dir..fdir.."/", ext)
+			ParseDir(n, dir..fdir.."/", ext, MaterialBox)
 			n.DoClick = function() end
 		end
 	end
+
 	for k,v in pairs(files) do
 		local pathExt = string.sub(v, -4)
 		local isValidExt = false
+
 		for _,y in pairs(ext) do
 			if pathExt == y then
 				isValidExt = true
+
 				break
 			end
 		end
+
 		if isValidExt then
 			local arq = string.sub(dir..v, 11, -5)
 			local n = t:AddNode(v)
+
 			n.Icon:SetImage("icon16/picture.png")
 			n.DoClick = function()
 				RunConsoleCommand("mapret_material", arq)
@@ -58,28 +62,30 @@ local function CreateMaterialBrowser()
 		Window:Center()
 		Window:SetPaintBackgroundEnabled(false)
 
-	MaterialBox = vgui.Create("DImage", Window)
-		MaterialBox:SetSize(Window:GetWide(), 0.41*(Window:GetTall()-25))
+	local MaterialBox = vgui.Create("DImage", Window)
+		MaterialBox:SetSize(Window:GetWide(), 0.41 * (Window:GetTall() - 25))
 		MaterialBox:SetPos(0, 25)
 
 	local function CreateList()
 		List = vgui.Create("DTree", Window)
-			List:SetSize(Window:GetWide(), 0.55*(Window:GetTall()-25))
-			List:SetPos(0, 0.41*(Window:GetTall()-25)+25)
+			List:SetSize(Window:GetWide(), 0.55 * (Window:GetTall() - 25))
+			List:SetPos(0, 0.41 * (Window:GetTall() - 25) + 25)
 			List:SetShowIcons(true)
 	end
+
 	CreateList()
 
 	local function FillList()
 		local node = List:AddNode("Materials!")
-		--ParseDir(node, "materials/", { ".vmt", ".png", ".jpg" })
-		ParseDir(node, "materials/", { ".vmt" })
+
+		--ParseDir(node, "materials/", { ".vmt", ".png", ".jpg" }, MaterialBox)
+		ParseDir(node, "materials/", { ".vmt" }, MaterialBox)
 		node:SetExpanded(true)
 	end
 
-	Reload = vgui.Create("DButton", Window)
-		Reload:SetSize(Window:GetWide(), 0.04*(Window:GetTall()-25))
-		Reload:SetPos(0, 0.96*(Window:GetTall()-25)+25)
+	local Reload = vgui.Create("DButton", Window)
+		Reload:SetSize(Window:GetWide(), 0.04 * (Window:GetTall() - 25))
+		Reload:SetPos(0, 0.96 * (Window:GetTall() - 25)  +25)
 		Reload:SetText("Reload List")
 		Reload.DoClick = function()
 			List:Remove()
@@ -94,6 +100,7 @@ local function ShowBrowser()
 	if not Window then 
 		CreateMaterialBrowser()
 	end
+
 	Window:SetVisible(true)
 	Window:MakePopup()
 end

cd52d29042a1168dcfdf07a2bf790bddaccd9460
Xalalau Xubilozo <xalalau@outlook.com>
2019-12-14 01:40:42 -0300

    Enhanced Material Browser
    New name
    Much better looks
    Make good use of screen space
    Texture preview adapts sizes correctly
    Invalid materials are no longer listed
    Added copy texture path to clipboard button
    The window doesn't lose its last state when it's closed

--- lua/autorun/client/materialbrowser.lua
+++ lua/autorun/client/materialbrowser.lua
@@ -1,7 +1,62 @@
-local Window
-local MatRetMaterial = CreateMaterial("MatRetMaterial", "UnlitGeneric", {["$basetexture"] = ""})
+local function UpdateMaterialBox(Window, MatRetMaterial, defaults)
+	-- Check if the draw is valid
+	if not Window or not MatRetMaterial then
+		return
+	end
 
-local function ParseDir(t, dir, ext, MaterialBox)
+	-- Resize material to the MaterialBox max size keeping the proportions
+	local texture = {
+		preview = {
+			["width"] = MatRetMaterial:Width(),
+			["height"] = MatRetMaterial:Height()
+		},
+		window = {
+			["width"] = defaults.materialBoxSize,
+			["height"] = defaults.materialBoxSize
+		}
+	}
+
+	local dimension
+
+	if texture.preview["width"] > texture.preview["height"] then
+		dimension = "width"
+	else
+		dimension = "height"
+	end
+
+	local proportion = texture.window[dimension] / texture.preview[dimension]
+
+	texture.preview["width"] = texture.preview["width"] * proportion
+	texture.preview["height"] = texture.preview["height"] * proportion
+
+	-- Get the window position
+	local positionX, positionY
+	positionX, positionY = Window:GetPos()
+
+	-- Draw Window background
+	draw.RoundedBox(8, positionX, positionY, Window:GetWide(), Window:GetTall(), Color(0, 0, 0, 200))
+
+	-- Draw MaterialBox background
+	draw.RoundedBox(0, positionX + defaults.border, positionY + defaults.border + defaults.topBar, defaults.materialBoxSize, defaults.materialBoxSize, Color(255, 255, 255, 150))
+
+	-- Get the relative image position
+	positionX = positionX + (defaults.materialBoxSize - texture.preview["width"]) / 2 + defaults.border
+	positionY = positionY + (defaults.materialBoxSize - texture.preview["height"]) / 2 + defaults.border + defaults.topBar
+
+	-- Draw image
+	surface.SetDrawColor(255, 255, 255, 255)
+	surface.SetMaterial(MatRetMaterial)
+	surface.DrawTexturedRect(positionX, positionY, texture.preview["width"], texture.preview["height"])
+
+	--[[
+	-- Note: old way to draw the preview. (Sadly it doesn't work with every texture)
+	MaterialBox:SetPos(positionX, positionY)
+	MaterialBox:SetMaterial(MatRetMaterial)
+	MaterialBox:SetSize(width, height)
+	]]
+end
+
+local function ParseDir(t, dir, ext, MatRetMaterial)
 	local files, dirs = file.Find(dir.."*", "GAME")
 
 	for _, fdir in pairs(dirs) do
@@ -9,7 +64,7 @@ local function ParseDir(t, dir, ext, MaterialBox)
 
 		n:SetExpanded(true)
 		n.DoClick = function()
-			ParseDir(n, dir..fdir.."/", ext, MaterialBox)
+			ParseDir(n, dir..fdir.."/", ext, MatRetMaterial)
 			n.DoClick = function() end
 		end
 	end
@@ -28,28 +83,25 @@ local function ParseDir(t, dir, ext, MaterialBox)
 
 		if isValidExt then
 			local arq = string.sub(dir..v, 11, -5)
-			local n = t:AddNode(v)
 
-			n.Icon:SetImage("icon16/picture.png")
-			n.DoClick = function()
-				RunConsoleCommand("mapret_material", arq)
-				if not Material(arq):IsError() then -- If the file is a .vmt
-					if Material(arq):GetTexture("$basetexture") then -- If the file has a $basetexture
-						MatRetMaterial:SetTexture("$basetexture", Material(arq):GetTexture("$basetexture"));
-					else
-						MatRetMaterial:SetTexture("$basetexture", Material("vgui/avatar_default"):GetTexture("$basetexture"));
-					end
-					MaterialBox:SetMaterial(MatRetMaterial)
-				else
-					MaterialBox:SetImage(arq..pathExt, "vgui/avatar_default") -- Shows every texture. Beautiful
-					-- MaterialBox:SetTexture("$basetexture",arq) -- Shows the textures that I can apply. Realistic
+			if not Material(arq):IsError() and Material(arq):GetTexture("$basetexture") then
+				local n = t:AddNode(v)
+
+				n.Icon:SetImage("icon16/picture.png")
+				n.DoClick = function()
+					RunConsoleCommand("mapret_material", arq)
+					MatRetMaterial:SetTexture("$basetexture", Material(arq):GetTexture("$basetexture"));
 				end
 			end
 		end
 	end
 end
 
+local Window
+
 local function CreateMaterialBrowser()
+	local MatRetMaterial = CreateMaterial("MatRetMaterial", "UnlitGeneric", {["$basetexture"] = "color"})
+
 	local topBar = 25
 	local border = 5
 	local buttonsHeight = 25
@@ -59,64 +111,71 @@ local function CreateMaterialBrowser()
 
 	local materialBoxSize = 4 * windowWidth/7 - border * 2
 
-	Window = vgui.Create("DFrame")
-		Window:SetTitle("Map Retexturizer Material Browser")
-		Window:SetSize(windowWidth, windowHeight)
-		Window:SetDeleteOnClose(false)
-		Window:SetIcon("icon16/picture.png")
-		Window:SetBackgroundBlur(true)
-		Window:Center()
-		Window:SetPaintBackgroundEnabled(false)
+	if not Window then
+		Window = vgui.Create("DFrame")
+			Window:SetTitle("Map Retexturizer Material Browser")
+			Window:SetSize(windowWidth, windowHeight)
+			Window:SetDeleteOnClose(false)
+			Window:SetIcon("icon16/picture.png")
+			Window:SetBackgroundBlur(true)
+			Window:Center()
+			Window:SetPaintBackgroundEnabled(false)
+			Window:SetVisible(true)
+			Window:MakePopup()
+			Window.Paint = function()
+			end
+			Window.Close = function()
+				hook.Remove("HUDPaint", "HUDPaint_MaterialBrowser")
+				Window:SetVisible(false)
+			end
 
-	local MaterialBox = vgui.Create("DImage", Window)
-		MaterialBox:SetSize(materialBoxSize, materialBoxSize)
-		MaterialBox:SetPos(border, border + topBar)
+		local MaterialBox = vgui.Create("DImage", Window)
+			MaterialBox:SetSize(materialBoxSize, materialBoxSize)
+			MaterialBox:SetPos(border, border + topBar)
 
-	local function CreateList()
-		List = vgui.Create("DTree", Window)
-			List:SetSize(Window:GetWide() - materialBoxSize - border * 3, Window:GetTall() - topBar - border * 2)
-			List:SetPos(materialBoxSize + border * 2, border + topBar)
-			List:SetShowIcons(true)
-	end
-
-	CreateList()
-
-	local function FillList()
-		local node = List:AddNode("Materials!")
-
-		--ParseDir(node, "materials/", { ".vmt", ".png", ".jpg" }, MaterialBox)
-		ParseDir(node, "materials/", { ".vmt" }, MaterialBox)
-		node:SetExpanded(true)
-	end
-
-	local Reload = vgui.Create("DButton", Window)
-		Reload:SetSize(materialBoxSize/2 - border/2, buttonsHeight)
-		Reload:SetPos(border, materialBoxSize + border * 2 + topBar)
-		Reload:SetText("Reload List")
-		Reload.DoClick = function()
-			List:Remove()
-			CreateList()
-			FillList()
+		local function CreateList()
+			List = vgui.Create("DTree", Window)
+				List:SetSize(Window:GetWide() - materialBoxSize - border * 3, Window:GetTall() - topBar - border * 2)
+				List:SetPos(materialBoxSize + border * 2, border + topBar)
+				List:SetShowIcons(true)
 		end
 
-	local Copy = vgui.Create("DButton", Window)
-		Copy:SetSize(materialBoxSize/2, buttonsHeight)
-		Copy:SetPos(border + materialBoxSize/2, materialBoxSize + border * 2 + topBar)
-		Copy:SetText("Copy to Clipboard")
-		Copy.DoClick = function()
-			SetClipboardText(MaterialBox:GetMaterial():GetTexture("$basetexture"):GetName())
+		CreateList()
+
+		local function FillList()
+			local node = List:AddNode("Materials!")
+
+			--ParseDir(node, "materials/", { ".vmt", ".png", ".jpg" }, MaterialBox)
+			ParseDir(node, "materials/", { ".vmt" }, MatRetMaterial)
+			node:SetExpanded(true)
 		end
 
-	FillList()
-end
+		local Reload = vgui.Create("DButton", Window)
+			Reload:SetSize(materialBoxSize/2 - border/2, buttonsHeight)
+			Reload:SetPos(border, materialBoxSize + border * 2 + topBar)
+			Reload:SetText("Reload List")
+			Reload.DoClick = function()
+				List:Remove()
+				CreateList()
+				FillList()
+			end
 
-local function ShowBrowser()
-	if not Window then 
-		CreateMaterialBrowser()
+		local Copy = vgui.Create("DButton", Window)
+			Copy:SetSize(materialBoxSize/2, buttonsHeight)
+			Copy:SetPos(border + materialBoxSize/2, materialBoxSize + border * 2 + topBar)
+			Copy:SetText("Copy to Clipboard")
+			Copy.DoClick = function()
+				SetClipboardText(MaterialBox:GetMaterial():GetTexture("$basetexture"):GetName())
+			end
+
+		FillList()
+	else
+		Window:SetVisible(true)
 	end
 
-	Window:SetVisible(true)
-	Window:MakePopup()
+	hook.Add("HUDPaint", "HUDPaint_MaterialBrowser", function()
+		UpdateMaterialBox(Window, MatRetMaterial, {materialBoxSize = materialBoxSize, topBar = topBar, border = border})
+	end)
 end
 
-concommand.Add("mapret_materialbrowser", ShowBrowser)
+concommand.Add("mapret_materialbrowser", CreateMaterialBrowser)

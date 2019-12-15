a90f054e2bce7b0bc3c1bce49cd1e03015b06e17
Xalalau Xubilozo <xalalau@outlook.com>
2019-12-15 03:15:32 -0300

    More options to send the files to the clients

--- lua/autorun/server/resource.lua
+++ lua/autorun/server/resource.lua
@@ -1,2 +1,17 @@
--- The clients need the map materials
-resource.AddWorkshop("1357913645")
+-- -----------------------------------
+-- The clients need the map materials!
+-- -----------------------------------
+
+-- The tool
+-- resource.AddWorkshop("1357913645")
+
+-- gma with all the materials
+resource.AddWorkshop("1937149388")
+
+-- Manually send each file
+--[[
+local files, _ = file.Find("materials/mapretexturizer/*.vmt", "GAME")
+for k, v in ipairs(files) do
+	resource.AddFile("materials/mapretexturizer/"..v)
+end
+]]

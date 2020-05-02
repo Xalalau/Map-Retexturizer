f91eed3384ab46fe82b2fc5bea791a906af8d864
Xalalau Xubilozo <xalalau@outlook.com>
2020-05-02 04:53:12 -0300

    Displacements init hack removed
    Apparently all the problems with darker materials were caused by the rendering of the skybox.

--- lua/mr/client/cl_mapmaterials.lua
+++ lua/mr/client/cl_mapmaterials.lua
@@ -4,14 +4,6 @@
 
 local MapMaterials = MR.MapMaterials
 
-local map = {
-	displacements = {
-		-- I reapply the grass materials before the first usage because they get darker after modified (Tool bug)
-		-- !!! Fix and remove it in the future !!!
-		hack = true
-	}
-}
-
 -- Networking
 net.Receive("MapMaterials:FixDetail_CL", function()
 	MapMaterials:FixDetail_CL(net.ReadString(), net.ReadBool())
@@ -85,34 +77,6 @@ function MapMaterials:Set_CL(data)
 		oldMaterial:SetString("$alpha", data.alpha)
 	end
 
-	-- Change the matrix
-	local textureMatrix = oldMaterial:GetMatrix("$basetexturetransform")
-
-	if data.rotation then
-		textureMatrix:SetAngles(Angle(0, data.rotation, 0)) 
-	end
-
-	if data.scalex and data.scaley then
-		textureMatrix:SetScale(Vector(1/data.scalex, 1/data.scaley, 1)) 
-	end
-
-	if data.offsetx and data.offsety then
-		textureMatrix:SetTranslation(Vector(data.offsetx, data.offsety)) 
-		oldMaterial:SetMatrix("$basetexturetransform", textureMatrix)
-	end
-
-	-- Change the detail
-	if data.detail then
-		if data.detail ~= "None" then
-			oldMaterial:SetTexture("$detail", MR.Materials:GetDetailList()[data.detail]:GetTexture("$basetexture"))
-			oldMaterial:SetString("$detailblendfactor", "1")
-		else
-			oldMaterial:SetString("$detailblendfactor", "0")
-			oldMaterial:SetString("$detail", "")
-			oldMaterial:Recompute()
-		end
-	end
-
 	--[[
 	-- Old tests that I want to keep here
 	mapMaterial:SetTexture("$bumpmap", Material(data.newMaterial):GetTexture("$basetexture"))
@@ -180,31 +144,10 @@ function MapMaterials.Displacements:Set_CL(displacement, newMaterial, newMateria
 		return false
 	end
 
-	-- Dirty hack: I reapply all the displacement materials because they get darker when modified by the tool
-	local delay = 0
-
-	if map.displacements.hack then
-		for k,v in pairs(MapMaterials.Displacements:GetDetected()) do
-			timer.Create("MRDiscplamentsDirtyHackCleanup"..tostring(delay), delay, 1, function()
-				net.Start("MapMaterials.Displacements:Set_SV")
-					net.WriteString(k)
-					net.WriteString(Material(k):GetTexture("$basetexture"):GetName())
-					net.WriteString(Material(k):GetTexture("$basetexture2"):GetName())
-				net.SendToServer()
-			end)
-			
-			delay = delay + 0.1
-		end
-
-		map.displacements.hack = false
-	end
-
 	-- Start the change
-	timer.Create("MRDiscplamentsDirtyHackAdjustment", delay + 0.1, 1, function() -- Wait for the initialization hack above
-		net.Start("MapMaterials.Displacements:Set_SV")
-			net.WriteString(displacement)
-			net.WriteString(newMaterial or "")
-			net.WriteString(newMaterial2 or "")
-		net.SendToServer()
-	end)
+	net.Start("MapMaterials.Displacements:Set_SV")
+		net.WriteString(displacement)
+		net.WriteString(newMaterial or "")
+		net.WriteString(newMaterial2 or "")
+	net.SendToServer()
 end

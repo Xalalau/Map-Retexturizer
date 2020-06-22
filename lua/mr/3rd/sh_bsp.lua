d9d30b9c6436f24ba13c2738372efa3059b8b25e
Xalalau Xubilozo <xalalau@outlook.com>
2020-06-22 04:32:15 -0300

    Enhanced BSP parser

--- lua/mr/3rd/sh_bsp.lua
+++ lua/mr/3rd/sh_bsp.lua
@@ -1,12 +1,28 @@
--- this was taken from 
--- http://steamcommunity.com/sharedfiles/filedetails/?id=160087429
--- which was made by Silverlan
--- http://steamcommunity.com/profiles/76561197967919092
--- which I copied for Map Retexturizer from
--- https://github.com/CapsAdmin/tod/blob/master/lua/tod/bsp.lua
--- Xalalau Xubilozo
+--[[
+	This was taken from 
+	http://steamcommunity.com/sharedfiles/filedetails/?id=160087429
+	which was made by Silverlan
+	http://steamcommunity.com/profiles/76561197967919092
+	And then was taken from 
+	https://github.com/CapsAdmin/tod/blob/master/lua/tod/bsp.lua
+	Which now is being used/improved for Map Retexturizer by Xalalau Xubilozo
+
+	Great sources to help us understand these interfaces:
+	https://github.com/ValveSoftware/source-sdk-2013/blob/master/sp/src/public/bspfile.h
+	https://csharp.hotexamples.com/site/file?hash=0x929844e2eaa63910340254a31bf9ff4c998856f7145967140a5d1e1b9c49345d&fullName=ProjectMoretz/BSPParser.cs&project=oxters168/ProjectMoretz
+	https://github.com/EstevanTH/GMod-map_manipulation_tool/blob/master/lua/autorun/map_manipulation_tool_api.lua
+
+	Changelog for Map Ratexturizer:
+		-- Minor syntax changes, merely visual
+		-- Enabled methods:ReadLumpDispInfo()
+		-- Added SIZEOF_UINT, toUInt() and ReadUInt()
+		-- Added ReadEntity(), BSP_LUMP_ENTITIES and methods:ReadEntities()
+		-- Added SIZE_LUMP_FACE, BSP_LUMP_FACES and methods:ReadLumpFaces()
+		-- Added the new functions to methods:ReadLump()
+]]
 
 local SIZEOF_INT = 4
+local SIZEOF_UINT = 4
 local SIZEOF_SHORT = 2
 local function toUShort(b)
 	local i = {string.byte(b,1,SIZEOF_SHORT)}
@@ -18,7 +34,12 @@ local function toInt(b)
 	if(i > 2147483647) then return i -4294967296 end
 	return i
 end
+local function toUInt(b)
+	local i = {string.byte(b,1,SIZEOF_UINT)}
+	return i[1] +i[2] *256 +i[3] *65536 +i[4] *16777216
+end
 local function ReadInt(f) return toInt(f:Read(SIZEOF_INT)) end
+local function ReadUInt(f) return toUInt(f:Read(SIZEOF_UINT)) end
 local function ReadUShort(f) return toUShort(f:Read(SIZEOF_SHORT)) end
 local function ReadShort(f)
 	local b1 = f:ReadByte()
@@ -40,6 +61,24 @@ local function ReadNullTerminatedString(f,max)
 	return t,l
 end
 
+local function ReadEntity(f,max)
+	local t = ""
+	local l
+	local i = 1
+	local c = f:Read(1)
+	if(c == "\0") then return "\0"; end
+	if(c ~= "{") then return "",i; end
+	while true do
+		t = t .. c
+		if(c == "}") then
+			l = i
+			break
+		end
+		c = f:Read(1)
+	end
+	return t,l
+end
+
 local HEADER_LUMPS = 64
 local SIZE_LUMP_PLANE = 20
 local SIZE_LUMP_BRUSH = 12
@@ -49,10 +88,13 @@ local SIZE_LUMP_TEXDATA = 32
 local SIZE_LUMP_TEXDATA_STRING_TABLE = 4
 local SIZE_LUMP_DISPINFO = 176
 local MAX_SIZE_TEXTURE_NAME = 128
+local SIZE_LUMP_FACE = 56
 
+BSP_LUMP_ENTITIES = 0
 BSP_LUMP_PLANES = 1
 BSP_LUMP_TEXDATA = 2
 BSP_LUMP_TEXINFO = 6
+BSP_LUMP_FACES = 7
 BSP_LUMP_BRUSHES = 18
 BSP_LUMP_BRUSHSIDES = 19
 BSP_LUMP_DISPINFO = 26
@@ -118,9 +160,46 @@ function methods:ReadLump(lumpID)
 	elseif(lumpID == BSP_LUMP_BRUSHSIDES) then return self:ReadLumpBrushSides()
 	elseif(lumpID == BSP_LUMP_TEXDATA_STRING_DATA) then return self:ReadLumpTextDataStringData()
 	elseif(lumpID == BSP_LUMP_TEXDATA_STRING_TABLE) then return self:ReadLumpTextDataStringTable()
-	elseif(lumpID == BSP_LUMP_DISPINFO) then return self:ReadLumpDispInfo() end
+	elseif(lumpID == BSP_LUMP_DISPINFO) then return self:ReadLumpDispInfo()
+	elseif(lumpID == BSP_LUMP_FACES) then return self:ReadLumpFaces()
+	end
 end
-/*
+
+function methods:ReadLumpFaces()
+	local f = self.m_file
+	if(!f) then return end
+	local info = self:GetLumpInfo(BSP_LUMP_FACES)
+	local faces = {}
+	f:Seek(info.fileofs)
+	local numFaces = info.filelen /SIZE_LUMP_FACE
+	for i = 1,numFaces do
+		table.insert(faces,{
+			planenum = ReadUShort(f),
+			side = f:ReadByte(),
+			onNode = f:ReadByte(),
+			firstedge = ReadInt(f),
+			numedges = ReadShort(f),
+			texinfo = ReadShort(f),
+			dispinfo = ReadShort(f),
+			surfaceFogVolumeID = ReadShort(f),
+			styles = { f:ReadByte(), f:ReadByte(), f:ReadByte(), f:ReadByte() },
+			lightofs = ReadInt(f),
+			area = f:ReadFloat(),
+			LightmapTextureMinsInLuxels = { ReadInt(f), ReadInt(f) },
+			LightmapTextureSizeInLuxels = { ReadInt(f), ReadInt(f) },
+			origFace = ReadInt(f),
+			numPrims = ReadUShort(f),
+			firstPrimID = ReadUShort(f),
+			smoothingGroups = ReadUInt(f)
+		})
+	end
+	return faces
+end
+
+-- Displacements loading:
+-- https://github.com/TheAlePower/TeamFortress2/blob/1b81dded673d49adebf4d0958e52236ecc28a956/tf2_src/engine/disp_mapload.cpp#L551
+-- Materiais estão aqui:
+-- https://github.com/TheAlePower/TeamFortress2/blob/1b81dded673d49adebf4d0958e52236ecc28a956/tf2_src/engine/disp_mapload.cpp#L628
 function methods:ReadLumpDispInfo()
 	local f = self.m_file
 	if(!f) then return end
@@ -129,34 +208,24 @@ function methods:ReadLumpDispInfo()
 	f:Seek(info.fileofs)
 	local numDisp = info.filelen /SIZE_LUMP_DISPINFO
 	for i = 1,numDisp do
-		local startPosition = Vector(f:ReadFloat(),f:ReadFloat(),f:ReadFloat())
-		local DispVertStart = ReadInt(f)
-		local DispTriStart = ReadInt(f)
-		local power = ReadInt(f)
-		local minTess = ReadInt(f)
-		local smoothingAngle = f:ReadFloat()
-		local contents = ReadInt(f)
-		local MapFace = ReadUShort(f)
-		local LightmapAlphaStart = ReadInt(f)
-		local LightmapSamplePositionStart = ReadInt(f)
-		local EdgeNeighbors = // TODO: Read these in properly. (See bspfile.h)
-		local CornerNeighbors = //
 		table.insert(dispinfo,{
-			startPosition = startPosition,
-			DispVertStart = DispVertStart,
-			DispTriStart = DispTriStart,
-			power = power,
-			minTess = minTess,
-			smoothingAngle = smoothingAngle,
-			contents = contents,
-			MapFace = MapFace,
-			LightmapAlphaStart = LightmapAlphaStart,
-			LightmapSamplePositionStart = LightmapSamplePositionStart
+			startPosition = Vector(f:ReadFloat(),f:ReadFloat(),f:ReadFloat()),
+			DispVertStart = ReadInt(f),
+			DispTriStart = ReadInt(f),
+			power = ReadInt(f),
+			minTess = ReadInt(f),
+			smoothingAngle = f:ReadFloat(),
+			contents = ReadInt(f),
+			MapFace = ReadUShort(f),
+			LightmapAlphaStart = ReadInt(f),
+			LightmapSamplePositionStart = ReadInt(f),
+			--EdgeNeighbors = // TODO: Read these in properly. (See bspfile.h)
+			--CornerNeighbors = //
 		})
 	end
 	return dispinfo
 end
-*/
+
 function methods:ReadLumpPlanes()
 	local f = self.m_file
 	if(!f) then return end
@@ -165,38 +234,13 @@ function methods:ReadLumpPlanes()
 	f:Seek(info.fileofs)
 	local numPlanes = info.filelen /SIZE_LUMP_PLANE
 	for i = 1,numPlanes do
-		local normal = Vector(f:ReadFloat(),f:ReadFloat(),f:ReadFloat())
-		local dist = f:ReadFloat()
-		local type = ReadInt(f)
 		table.insert(planes,{
-			normal = normal,
-			dist = dist,
-			type = type
+			normal = Vector(f:ReadFloat(),f:ReadFloat(),f:ReadFloat()),
+			dist = f:ReadFloat(),
+			type = ReadInt(f)
 		})
 	end
 	return planes
-end
-
-function methods:ReadLumpBrushSides()
-	local f = self.m_file
-	if(!f) then return end
-	local info = self:GetLumpInfo(BSP_LUMP_BRUSHSIDES)
-	local brushSides = {}
-	f:Seek(info.fileofs)
-	local numBrushSides = info.filelen /SIZE_LUMP_BRUSHSIDE
-	for i = 1,numBrushSides do
-		local planenum = ReadUShort(f)
-		local texinfo = ReadShort(f)
-		local dispinfo = ReadShort(f)
-		local bevel = ReadShort(f)
-		table.insert(brushSides,{
-			planenum = planenum,
-			texinfo = texinfo,
-			dispinfo = dispinfo,
-			bevel = bevel
-		})
-	end
-	return brushSides
 end
 
 function methods:ReadLumpBrushes()
@@ -207,17 +251,53 @@ function methods:ReadLumpBrushes()
 	f:Seek(info.fileofs)
 	local numBrushs = info.filelen /SIZE_LUMP_BRUSH
 	for i = 1,numBrushs do
-		local firstside = ReadInt(f)
-		local numsides = ReadInt(f)
-		local contents = ReadInt(f)
-		local brush = {
-			firstside = firstside,
-			numsides = numsides,
-			contents = contents
-		}
-		table.insert(brushs,brush)
+		table.insert(brushs,{
+			firstside = ReadInt(f),
+			numsides = ReadInt(f),
+			contents = ReadInt(f)
+		})
 	end
 	return brushs
+end
+
+function methods:ReadLumpBrushSides()
+	local f = self.m_file
+	if(!f) then return end
+	local info = self:GetLumpInfo(BSP_LUMP_BRUSHSIDES)
+	local brushSides = {}
+	f:Seek(info.fileofs)
+	local numBrushSides = info.filelen /SIZE_LUMP_BRUSHSIDE
+	for i = 1,numBrushSides do
+		table.insert(brushSides,{
+			planenum = ReadUShort(f),
+			texinfo = ReadShort(f),
+			dispinfo = ReadShort(f),
+			bevel = ReadShort(f)
+		})
+	end
+	return brushSides
+end
+
+function methods:ReadEntities()
+	local f = self.m_file
+	if(!f) then return end
+	local info = self:GetLumpInfo(BSP_LUMP_ENTITIES)
+	local entities = {}
+	f:Seek(info.fileofs)
+	local sz = info.filelen
+	local max = sz
+	local i = 1
+	while(sz > 0) do
+		local t,l = ReadEntity(f, max)
+		if t == "\0" then
+			break
+		elseif t ~= "" then
+			table.insert(entities,t)
+			i = i + 1
+		end
+		sz = sz - l
+	end
+	return entities
 end
 
 function methods:ReadLumpTexInfo()
@@ -242,13 +322,11 @@ function methods:ReadLumpTexInfo()
 				lightmapVecs[i][j] = f:ReadFloat()
 			end
 		end
-		local flags = ReadInt(f)
-		local texdata = ReadInt(f)
 		table.insert(texinfo,{
 			textureVecs = textureVecs,
 			lightmapVecs = lightmapVecs,
-			flags = flags,
-			texdata = texdata
+			flags = ReadInt(f),
+			texdata = ReadInt(f)
 		})
 	end
 	return texinfo
@@ -262,19 +340,13 @@ function methods:ReadLumpTexData()
 	f:Seek(info.fileofs)
 	local numTexData = info.filelen /SIZE_LUMP_TEXDATA
 	for i = 1,numTexData do
-		local reflectivity = Vector(f:ReadFloat(),f:ReadFloat(),f:ReadFloat())
-		local nameStringTableID = ReadInt(f)
-		local width = ReadInt(f)
-		local height = ReadInt(f)
-		local view_width = ReadInt(f)
-		local view_height = ReadInt(f)
 		table.insert(texdata,{
-			reflectivity = reflectivity,
-			nameStringTableID = nameStringTableID,
-			width = wdith,
-			height = height,
-			view_width = view_width,
-			view_height = view_height
+			reflectivity = Vector(f:ReadFloat(),f:ReadFloat(),f:ReadFloat()),
+			nameStringTableID = ReadInt(f),
+			width = ReadInt(f),
+			height = ReadInt(f),
+			view_width = ReadInt(f),
+			view_height = ReadInt(f)
 		})
 	end
 	return texdata
@@ -308,7 +380,7 @@ function methods:ReadLumpTextDataStringTable()
 	return texdatastring
 end
 
-function methods:GetTranslatedTextDataStringTable()
+function methods:GetTranslatedTextDataStringTable(filter)
 	local f = self.m_file
 	if(!f) then return end
 	local data = {}

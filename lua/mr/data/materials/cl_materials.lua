ff83ec4c326bb9af70aea202151621d75b94e546
Xalalau Xubilozo <xalalau@outlook.com>
2020-07-23 15:12:56 -0300

    Bring the preview to the front if it changes in the context menu

--- lua/mr/data/materials/cl_materials.lua
+++ lua/mr/data/materials/cl_materials.lua
@@ -83,9 +83,11 @@ function Materials:SetPreview(newData, isDecal)
 		MR.CL.Map:Set(newData)
 
 		if MR.CL.PPanel:Preview_GetSelf() then
+			MR.CL.PPanel:Hide()
 			MR.CL.PPanel:Preview_GetSelf():Remove()
 			timer.Create("MRSetPreview", 0.01, 1, function()
 				MR.CL.PPanel:SetPreviewBox() 
+				MR.CL.PPanel:Show()
 			end)
 		end
 	end

dae487a0545cbe4ca82c281eb1cc476e7fa57ae9
Xalalau Xubilozo <xalalau@outlook.com>
2020-07-25 16:43:01 -0300

    Fixed some missing arguments

--- lua/mr/data/duplicator/sv_duplicator.lua
+++ lua/mr/data/duplicator/sv_duplicator.lua
@@ -441,12 +441,12 @@ function Duplicator:Finish(ply, isGModLoadOverriding)
 		-- Finish for new players
 		if ply ~= MR.SV.Ply:GetFakeHostPly() and MR.Ply:GetFirstSpawn(ply) and not isGModLoadOverriding then
 			-- Start a new (partial) load if modifications were made while the player was entering
-			if MR.Ply:GetNewDupTable() then
+			if MR.Ply:GetNewDupTable(ply) then
 				-- Create a copy
-				local newSavedTable = table.Copy(MR.Ply:GetNewDupTable())
+				local newSavedTable = table.Copy(MR.Ply:GetNewDupTable(ply))
 
 				-- Empty the original table, so we can repeat this process if it's necessary
-				for k,v in pairs(MR.Ply:GetNewDupTable()) do
+				for k,v in pairs(MR.Ply:GetNewDupTable(ply)) do
 					if not k == "savingFormat" then
 						table.Empty(v)
 					end

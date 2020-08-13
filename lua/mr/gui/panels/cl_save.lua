2ed0bd4dd585104f4868e6734dfcc69df4692871
Xalalau Xubilozo <xalalau@outlook.com>
2020-08-13 19:26:41 -0300

    Readded autosave box to save section with a red background

--- lua/mr/gui/panels/cl_save.lua
+++ lua/mr/gui/panels/cl_save.lua
@@ -34,11 +34,23 @@ function Panels:SetSave(parent, frameType, info)
 		y = saveTextInfo.y + saveTextInfo.height + MR.CL.Panels:GetGeneralBorders()
 	}
 
+	local panel2Info = {
+		width = 81,
+		height = MR.CL.Panels:GetTextHeight(),
+		x = saveButtonInfo.x,
+		y = saveButtonInfo.y + saveButtonInfo.height + MR.CL.Panels:GetGeneralBorders()
+	}
+
+	local autosaveBoxInfo = {
+		x = MR.CL.Panels:GetGeneralBorders(),
+		y = MR.CL.Panels:GetGeneralBorders()
+	}
+
 	local saveDLabelHintInfo = {
 		width = width - MR.CL.Panels:GetGeneralBorders() * 2,
 		height = MR.CL.Panels:GetTextHeight(),
-		x = saveButtonInfo.x + MR.CL.Panels:GetTextMarginLeft(),
-		y = saveButtonInfo.y + saveButtonInfo.height - MR.CL.Panels:GetGeneralBorders()
+		x = panel2Info.x + 100,
+		y = panel2Info.y - MR.CL.Panels:GetGeneralBorders() - 3
 	}
 
 	--------------------------
@@ -56,15 +68,6 @@ function Panels:SetSave(parent, frameType, info)
 		saveText:SetConVar("internal_mr_savename")
 
 	--------------------------
-	-- Save hint
-	--------------------------
-	local saveDLabelHint = vgui.Create("DLabel", panel)
-		saveDLabelHint:SetPos(saveDLabelHintInfo.x, saveDLabelHintInfo.y)
-		saveDLabelHint:SetSize(saveDLabelHintInfo.width, saveDLabelHintInfo.height)
-		saveDLabelHint:SetText("\nChanged models aren't stored!")
-		saveDLabelHint:SetTextColor(MR.CL.Panels:GetHintColor())
-
-	--------------------------
 	-- Save button
 	--------------------------
 	local saveButton = vgui.Create("DButton", panel)
@@ -75,6 +78,51 @@ function Panels:SetSave(parent, frameType, info)
 		saveButton.DoClick = function()
 			MR.CL.Save:Set()
 		end
+
+	--------------------------
+	-- Autosave
+	--------------------------
+	local panel2 = vgui.Create("DPanel", panel)
+		panel2:SetSize(panel2Info.width, panel2Info.height)
+		panel2:SetPos(panel2Info.x, panel2Info.y)
+		panel2:SetBackgroundColor(Color(255, 180, 180, 255))
+
+		local autosaveBox = vgui.Create("DCheckBoxLabel", panel2)
+			MR.Sync:Set(autosaveBox, "save", "box")
+			autosaveBox:SetPos(autosaveBoxInfo.x, autosaveBoxInfo.y)
+			autosaveBox:SetText("Autosave")
+			autosaveBox:SetTextColor(Color(0, 0, 0, 255))
+			autosaveBox:SetValue(true)
+			autosaveBox.OnChange = function(self, val)
+				-- Force the field to update and disable a sync loop block
+				if MR.CL.Sync:GetLoopBlock() then
+					if val ~= autosaveBox:GetValue() then
+						autosaveBox:SetChecked(val)
+					else
+						MR.CL.Sync:SetLoopBlock(false)
+					end
+			
+					return
+				-- Admin only: reset the option if it's not being synced and return
+				elseif not MR.Ply:IsAdmin(LocalPlayer()) then
+					autosaveBox:SetChecked(GetConVar("internal_mr_autosave"):GetBool())
+			
+					return
+				end
+			
+				net.Start("SV.Save:SetAuto")
+					net.WriteBool(val)
+				net.SendToServer()
+			end
+
+	--------------------------
+	-- Save hint
+	--------------------------
+	local saveDLabelHint = vgui.Create("DLabel", panel)
+		saveDLabelHint:SetPos(saveDLabelHintInfo.x, saveDLabelHintInfo.y)
+		saveDLabelHint:SetSize(saveDLabelHintInfo.width, saveDLabelHintInfo.height)
+		saveDLabelHint:SetText("\nChanged models aren't stored!")
+		saveDLabelHint:SetTextColor(MR.CL.Panels:GetHintColor())
 
 	-- Margin bottom
 	local extraBorder = vgui.Create("DPanel", panel)

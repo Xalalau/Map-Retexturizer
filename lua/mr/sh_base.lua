5a006806a21d4c294ee722abf33a426618648a3d
Xalalau Xubilozo <xalalau@outlook.com>
2020-08-13 13:27:51 -0300

    Moved the cvars to sh_base

--- lua/mr/sh_base.lua
+++ lua/mr/sh_base.lua
@@ -6,6 +6,36 @@ local Base = {}
 Base.__index = Base
 MR.Base = Base
 
+do
+	local sh_flags = { FCVAR_REPLICATED }
+
+	CreateConVar("internal_mr_admin", "1", sh_flags)
+	CreateConVar("internal_mr_autosave", "1", sh_flags)
+	CreateConVar("internal_mr_autoload", "", sh_flags)
+	CreateConVar("internal_mr_skybox", "", sh_flags)
+	CreateConVar("internal_mr_delay", "0.035", sh_flags)
+	CreateConVar("internal_mr_duplicator_cleanup", "1", sh_flags)
+	CreateConVar("internal_mr_skybox_toolgun", "1", sh_flags)
+end
+
+do
+	local cl_flags = { FCVAR_CLIENTDLL, FCVAR_USERINFO }
+
+	CreateConVar("internal_mr_decal", "0", cl_flags)
+	CreateConVar("internal_mr_displacement", "", cl_flags)
+	CreateConVar("internal_mr_savename", "", cl_flags)
+	CreateConVar("internal_mr_new_material", "dev/dev_measuregeneric01b", cl_flags)
+	CreateConVar("internal_mr_old_material", "", cl_flags)
+	CreateConVar("internal_mr_detail", "None", cl_flags)
+	CreateConVar("internal_mr_alpha", "1", cl_flags)
+	CreateConVar("internal_mr_offsetx", "0", cl_flags)
+	CreateConVar("internal_mr_offsety", "0", cl_flags)
+	CreateConVar("internal_mr_scalex", "1", cl_flags)
+	CreateConVar("internal_mr_scaley", "1", cl_flags)
+	CreateConVar("internal_mr_rotation", "0", cl_flags)
+end
+
+
 local base = {
 	version = "GitHub, Version 16.1+",
 	materials = {

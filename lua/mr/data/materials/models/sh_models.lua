1f45ef3e4cb2faa1b89ff1d5a061f8d123e82b42
Xalalau Xubilozo <xalalau@outlook.com>
2021-05-26 00:43:09 -0300

    Add collision resizer

--- lua/mr/data/materials/models/sh_models.lua
+++ lua/mr/data/materials/models/sh_models.lua
@@ -198,3 +198,32 @@ function Models:Remove(ply, ent, isBroadcasted)
 
 	return true
 end
+
+-- Resize collision model
+-- Code ported from Collision Resizer (ENHANCED), made by Tau
+function Models:IsValidPhysicsObject(physobj)
+	return TypeID(physobj) == TYPE_PHYSOBJ and physobj:IsValid()
+end
+
+function Models:ResizePhysics(ent, scale)
+	ent:PhysicsInit(SOLID_VPHYSICS)
+
+	local physobj = ent:GetPhysicsObject()
+
+	if not Models:IsValidPhysicsObject(physobj) then return false end
+
+	local physmesh = physobj:GetMeshConvexes()
+
+	if not istable(physmesh) or #physmesh < 1 then return false end
+
+	for convexkey, convex in pairs(physmesh) do
+		for poskey, postab in pairs(convex) do
+			convex[poskey] = postab.pos * scale
+		end
+	end
+
+	ent:PhysicsInitMultiConvex(physmesh)
+	ent:EnableCustomCollisions(true)
+
+	return Models:IsValidPhysicsObject(ent:GetPhysicsObject())
+end

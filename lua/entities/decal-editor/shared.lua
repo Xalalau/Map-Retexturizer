f7439199c212a15892716479e754b1bfe3b7de9e
Xalalau Xubilozo <xalalau@outlook.com>
2021-06-03 21:01:53 -0300

    Move decal-editor initialization back to server

--- lua/entities/decal-editor/shared.lua
+++ lua/entities/decal-editor/shared.lua
@@ -5,18 +5,3 @@ ENT.Author = "Xalalau"
 ENT.Spawnable = true
 ENT.AdminSpawnable = false
 ENT.BlockStencil = false
-
-function ENT:Initialize()
-	if SERVER then
-        self:SetModel("models/hunter/plates/plate1x1.mdl")
-        self:SetTrigger(true)
-        self:PhysicsInit(SOLID_VPHYSICS)
-        self:SetSolid(SOLID_VPHYSICS)
-        self:SetCollisionGroup(COLLISION_GROUP_WORLD)
-        self:SetMaterial("concrete/concretefloor004")
-        self:SetRenderMode(RENDERMODE_TRANSALPHA)
-        self:SetColor(Color(255, 0, 0, 255))
-        self:DrawShadow(false)
-        self.inFocus = false
-    end
-end

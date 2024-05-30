8eeab048ecd80d096fcd510881dfc6af71133407
Xalalau <xalalau@outlook.com>
2024-05-30 00:23:39 -0300

    Add SEv.Material:FixVertexLitMaterial

--- lua/mr/data/materials/cl_materials.lua
+++ lua/mr/data/materials/cl_materials.lua
@@ -170,6 +170,38 @@ function Materials:SetPreview(newData)
 	end
 end
 
+-- Great to fix menu images flickering etc
+function SEv.Material:FixVertexLitMaterial(materialName)
+    local material = Material(materialName)
+
+    if not material then return material end
+ 
+    local strImage = material:GetName() .. "_fixed"
+
+    if string.find(material:GetShader(), "VertexLitGeneric") or string.find(material:GetShader(), "Cable") then
+        local materialFixed = Material(strImage)
+
+        if not materialFixed:IsError() then return materialFixed end
+
+        local texture = material:GetString("$basetexture")
+
+        if texture then
+            local translucent = bit.band(material:GetInt("$flags"), 2097152)
+
+            local params = {}
+            params[ "$basetexture" ] = texture
+
+            if translucent == 2097152 then
+                params[ "$translucent" ] = 1
+            end
+
+            material = CreateMaterial(strImage, "VertexLitGeneric", params)
+        end
+    end
+
+    return material
+end
+
 -- Apply the changes of a Data table
 function Materials:Apply(data)
 	if not data then return end
